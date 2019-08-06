893f926c5a14c6e7938ed6c40d51279bace058e9
try1975 <try1975@mail.ru>
2019-08-06 21:33:53 +0300

    add regions to dictionary

--- GzCommon/RegionItem.cs
+++ GzCommon/RegionItem.cs
@@ -15,10 +15,10 @@ namespace GzCommon
         {
             return new[]
             {
-                new RegionItem {Name = "Республика Адыгея", Code = "01"},
-                new RegionItem {Name = "Республика Алтай", Code = "04"},
+                new RegionItem {Name = "Республика Адыгея", Code = "01"}, 
                 new RegionItem {Name = "Республика Башкортостан", Code = "02"},
                 new RegionItem {Name = "Республика Бурятия", Code = "03"},
+                new RegionItem {Name = "Республика Алтай", Code = "04"},
                 new RegionItem {Name = "Республика Дагестан", Code = "05"},
                 new RegionItem {Name = "Республика Ингушетия", Code = "06"},
                 new RegionItem {Name = "Кабардино-Балкарская республика", Code = "07"},
@@ -26,7 +26,6 @@ namespace GzCommon
                 new RegionItem {Name = "Карачаево-Черкесская республика", Code = "09"},
                 new RegionItem {Name = "Республика Карелия", Code = "10"},
                 new RegionItem {Name = "Республика Коми", Code = "11"},
-                new RegionItem {Name = "Республика Крым", Code = "91"},
                 new RegionItem {Name = "Республика Марий Эл", Code = "12"},
                 new RegionItem {Name = "Республика Мордовия", Code = "13"},
                 new RegionItem {Name = "Республика Саха (Якутия)", Code = "14"},
@@ -38,18 +37,21 @@ namespace GzCommon
                 new RegionItem {Name = "Чеченская республика", Code = "20"},
                 new RegionItem {Name = "Чувашская республика", Code = "21"},
                 new RegionItem {Name = "Алтайский край", Code = "22"},
-                new RegionItem {Name = "Забайкальский край", Code = "75"},
-                new RegionItem {Name = "Камчатский край", Code = "41"},
                 new RegionItem {Name = "Краснодарский край", Code = "23"},
                 new RegionItem {Name = "Красноярский край", Code = "24"},
-                new RegionItem {Name = "Пермский край", Code = "59"},
                 new RegionItem {Name = "Приморский край", Code = "25"},
                 new RegionItem {Name = "Ставропольский край", Code = "26"},
                 new RegionItem {Name = "Хабаровский край", Code = "27"},
                 new RegionItem {Name = "Амурская область", Code = "28"},
+                new RegionItem {Name = "Архангельская область", Code = "29"},
+                new RegionItem {Name = "Астраханская область", Code = "30"},
+                new RegionItem {Name = "Камчатский край", Code = "41"},
 
                 new RegionItem {Name = "Московская область", Code = "50"},
-                new RegionItem {Name = "Москва", Code = "77"}
+                new RegionItem {Name = "Пермский край", Code = "59"},
+                new RegionItem {Name = "Забайкальский край", Code = "75"},
+                new RegionItem {Name = "Москва", Code = "77"},
+                new RegionItem {Name = "Республика Крым", Code = "91"},
             };
         }
     }
